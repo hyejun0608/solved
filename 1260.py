d9cc7484e8d2ea3821fbcdfb89b73b63744cda88
hyejun0608 <hyejun0608@naver.com>
2026-04-05 16:57:53 +0900

    1260번: DFS와 BFS

---
+++ 1260.py
@@ -0,0 +1,48 @@
+# https://www.acmicpc.net/problem/1260
+#   1260번: DFS와 BFS
+
+from queue import Queue;
+
+# input 입력받기
+N, M, V = map(int, "5 5 3".split())
+
+# graph 리스트 만들기
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    X, Y = map(int, input().split())
+    graph[X].append(Y)
+    graph[Y].append(X)
+
+dfs = []
+bfs = []
+
+# DFS
+def DFS(edge):
+  dfs.append(str(edge))
+
+  for a in sorted(graph[edge]):
+    if str(a) not in dfs:
+      DFS(a)
+
+DFS(V)
+
+# BFS
+q = Queue()
+q.put(V)
+visited = set()
+
+while not q.empty():
+  edge = q.get()
+
+  if edge not in visited:
+    visited.add(edge)
+    bfs.append(str(edge))
+
+  for a in sorted(graph[edge]):
+    if a not in visited:
+      q.put(a)
+
+# 정답 출력
+print(" ".join(dfs))
+print(" ".join(bfs))
